d73d37eef1957139da95e2a829dbc6e3938baa99
wackyturtle <wcytutl01@gmail.com>
2025-06-22 15:43:56 +0900

    다시 원상복구

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.thread_app_sample"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
